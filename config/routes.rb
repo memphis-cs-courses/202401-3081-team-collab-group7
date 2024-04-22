0d4c592a03808e4b5092745f966544a7a2f66747
Jacob Hensley <jacobdlhensley@gmail.com>
2024-04-22 14:47:31 -0500

    Add JacobHensley page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+  get 'JacobHensley', to: 'pages#JacobHensley', as: 'JacobHensley'
 end
